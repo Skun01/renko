c84101222a265db0ec5575fefc22f00fc69121be
Skun01 <truongg9655@gmail.com>
2025-11-05 21:40:05 +0700

    create custom SaveChangesAsync method in DbContext

--- Data/ProjectZContext.cs
+++ Data/ProjectZContext.cs
@@ -10,6 +10,21 @@ public class ProjectZContext : DbContext
     public DbSet<User> Users { set; get; }
     public DbSet<Role> Roles { set; get; }
     public ProjectZContext(DbContextOptions<ProjectZContext> options) : base(options) { }
+    
+    // Custom SaveChangesAsync
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+            entry.Entity.UpdatedAt = DateTime.UtcNow;
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
